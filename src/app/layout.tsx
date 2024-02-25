b76a478a4b03037447f237138b174e24a3af66cf
flovis <floviskabndafkm@gmail.com>
2024-02-26 01:58:12 +0200

    add clec

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -20,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-urbanist antialiased", urbanist.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+      <html lang="en">
+        <body className={cn("font-urbanist antialiased", urbanist.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
